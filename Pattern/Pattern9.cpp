8dde19087c11926b4ce5385bfaaa3a0d07d4a4de
Deepak Yadav <deepak.yadav270300@gmail.com>
2023-08-08 23:49:22 +0530

    Pattern 9 another approach

--- Pattern/Pattern9.cpp
+++ Pattern/Pattern9.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 using namespace std;
+// int main()
+// {
+//     int n, i = 1;
+//     cout << "Enter the value of n: \n";
+//     cin >> n;
+//     while (i <= n)
+//     {
+//         int j = 1;
+//         int value = i;
+//         while (j <= i)
+//         {
+//             cout << value;
+//             value = value - 1;
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+// }
+
+// another approach without using the extra variable i.e value in this case
 int main()
 {
     int n, i = 1;
@@ -8,11 +29,9 @@ int main()
     while (i <= n)
     {
         int j = 1;
-        int value = i;
         while (j <= i)
         {
-            cout << value;
-            value = value - 1;
+            cout << i - j + 1;
             j++;
         }
         cout << endl;
