33c18d58bdcc7d9288205b05dcd847f444bc28f3
Deepak Yadav <deepak.yadav270300@gmail.com>
2023-08-10 00:05:11 +0530

    Pattern 11 modified

--- Pattern/Pattern11.cpp
+++ Pattern/Pattern11.cpp
@@ -1,3 +1,8 @@
+// Desired Pattern :
+// A B C D
+// E F G H
+// I J K L
+// M N O P
 #include <iostream>
 using namespace std;
 int main()
@@ -12,7 +17,9 @@ int main()
         while (j <= n)
         {
             cout << ch;
-            char ch = ch + 1;
+            // the below line of code declares the variable ch again and its scope is limited to only the while loop so the expected output is not same as the actual output
+            // char ch = ch + 1;
+            ch = ch + 1;
             j++;
         }
         cout << endl;
