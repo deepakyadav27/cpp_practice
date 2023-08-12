fa9424875c5cac66d5d47f2e453c3187a8e787b2
Deepak Yadav <deepak.yadav270300@gmail.com>
2023-08-12 13:37:06 +0530

    Pattern 17 approach 2 done

--- Pattern/Pattern17.cpp
+++ Pattern/Pattern17.cpp
@@ -5,6 +5,34 @@
 //  ****
 
 // Approach 1
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int i = 1, n;
+//     cout << "Enter the value of n: \n";
+//     cin >> n;
+//     while (i <= n)
+//     {
+//         int j = 1;
+//         while (j <= n)
+//         {
+//             if (j <= n - i)
+//             {
+//                 cout << " ";
+//             }
+//             else
+//             {
+//                 cout << "*";
+//             }
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+// }
+
+// Approach 2
 #include <iostream>
 using namespace std;
 int main()
@@ -12,22 +40,23 @@ int main()
     int i = 1, n;
     cout << "Enter the value of n: \n";
     cin >> n;
+    // to print the whitespace
     while (i <= n)
     {
-        int j = 1;
-        while (j <= n)
+        int space = n - i;
+        while (space)
         {
-            if (j <= n - i)
-            {
-                cout << " ";
-            }
-            else
-            {
-                cout << "*";
-            }
+            cout << " ";
+            space--;
+        }
+        // to print the stars
+        int j = 1;
+        while (j <= i)
+        {
+            cout << "*";
             j++;
         }
-        cout << endl;
         i++;
+        cout << endl;
     }
 }
